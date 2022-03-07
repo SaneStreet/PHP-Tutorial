3ccb5426fb3469b902df8f7125b90ebbdc0c1759
SaneStreet <MichaelHansenIT@live.dk>
2022-03-07 12:16:03 +0100

    Added XSS measures

---
+++ add.php
@@ -0,0 +1,37 @@
+<?php
+
+    // if(isset($_GET['submit'])){
+    //     echo $_GET['email'];
+    //     echo $_GET['title'];
+    //     echo $_GET['ingredients'];
+    // }
+    
+    if(isset($_POST['submit'])){
+        echo htmlspecialchars($_POST['email']);
+        echo htmlspecialchars($_POST['title']);
+        echo htmlspecialchars($_POST['ingredients']);
+    }
+?>
+
+<!DOCTYPE html>
+<html>
+    <?php include 'templates/header.php'; ?>
+
+    <section class="container grey-text">
+        <h4 class="center">Add a Pizza</h4>
+        <form action="add.php" method="POST" class="white">
+            <label for="">Email:</label>
+            <input type="text" name="email">
+            <label for="">Pizza titel:</label>
+            <input type="text" name="title">
+            <label for="">Ingredients (comma separated):</label>
+            <input type="text" name="ingredients">
+            <div class="center">
+                <input type="submit" name="submit" value="submit" class="btn brand z-depth-0">
+            </div>
+        </form>
+    </section>
+
+    <?php include 'templates/footer.php'; ?>
+    
+</html>
