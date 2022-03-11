3e001616f57378b085750ad8de05279cf687cbd3
SaneStreet <MichaelHansenIT@live.dk>
2022-03-11 13:42:51 +0100

    Added Superglobals chapter

---
+++ chapters/superglobals.php
@@ -0,0 +1,28 @@
+<?php 
+
+    ## Superglobals
+
+    # Ex: $_GET['name'] or $_POST['id']
+
+    // $_SERVER superglobal
+
+    echo $_SERVER['SERVER_NAME'] . '<br />';
+
+    echo $_SERVER['REQUEST_METHOD'] . '<br />';
+
+    echo $_SERVER['SCRIPT_FILENAME'] . '<br />';
+
+    echo $_SERVER['PHP_SELF'] . '<br />';
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>PHP Tutorial - The Net Ninja</title>
+    </head>
+
+    <body>
+
+    </body>
+</html>
