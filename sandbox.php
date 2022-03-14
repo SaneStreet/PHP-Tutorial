9cde70c404682a988cef50bb352da1c1fc826541
SaneStreet <MichaelHansenIT@live.dk>
2022-03-14 12:34:14 +0100

    Chapter File System - Part 2

--- sandbox.php
+++ sandbox.php
@@ -1,5 +1,31 @@
 <?php 
 
-    
+    ## File System - Part 2
+
+    $file = 'quotes.txt';
+
+    # opening a file for reading
+    $handle = fopen($file, 'a+');
+    // for more fopen() functions:
+    // https://www.w3schools.com/php/func_filesystem_fopen.asp
+
+    # read the file
+    // echo fread($handle, filesize($file));
+    // echo fread($handle, 112);
+
+    # read a single line
+    // echo fgets($handle);
+    // echo fgets($handle);
+
+    # read a single character
+    // echo fgetc($handle);
+
+    # writing to a file
+    // fwrite($handle, "\nEverything popular is wrong.");
+
+    # closes the handling of the file
+    fclose($handle);
+
+    // unlink($file); // deletes the file
 
 ?>
